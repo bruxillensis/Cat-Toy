135f1a10474132b0d969a7c51930b418ad3f863c
bpoulse <bpoulse@uwo.ca>
2017-12-17 22:45:49 -0500

    adding to index

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const Toy = require('CatToy')
+
 console.log('-------------------------------------------------------');
 console.log('--                      Cat Toy                      --');
 console.log('-------------------------------------------------------');
 
+toy = new Toy();
+toy.play();
